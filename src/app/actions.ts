a8947b65aa27e3ee9ef87cb89222da7b097942d7
István Antal <istvan@antal.xyz>
2024-06-03 14:19:52 +0100

    Make data loading a server action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,6 +10,17 @@ const connectToDatabase = async () => {
     return db;
 };
 
+export async function findJobsForBoard() {
+    const collection = (await connectToDatabase()).collection('jobs');
+
+    return (await collection.find({ hidden: { $not: { $eq: true } } }, { sort: [['created', 'desc']] }).toArray()).map(
+        item => ({
+            ...item,
+            _id: item._id.toJSON(),
+        }),
+    ) as unknown as Job[];
+}
+
 export async function dismissJob(job: Job) {
     const collection = (await connectToDatabase()).collection('jobs');
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
-import { MongoClient } from 'mongodb';
-import type { Job } from '../lib/job';
+import { findJobsForBoard } from './actions';
 import { JobList } from './job-list';
 
 export default async function Home() {
-    // TODO: remove repetition
-    const url = 'mongodb://localhost:27017';
-    const client = new MongoClient(url);
-    await client.connect();
-
-    const db = client.db('job_hunter');
-    const collection = db.collection('jobs');
-
-    const jobs = (
-        await collection.find({ hidden: { $not: { $eq: true } } }, { sort: [['created', 'desc']] }).toArray()
-    ).map(item => ({
-        ...item,
-        _id: item._id.toJSON(),
-    })) as unknown as Job[];
+    const jobs = await findJobsForBoard();
 
     return <JobList jobs={jobs} />;
 }
